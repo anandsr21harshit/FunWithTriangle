5c8377cdb55778fc0a478b01319ee9743761c52a
Harshit Anand <harshitanand97@gmail.com>
2021-08-18 10:30:49 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const quizform = document.querySelector(".quiz-form");
+const submitBtn = document.querySelector("#submit");
+const output = document.querySelector(".output");
+
+const answers = [
+    "90°",
+    "one right angle",
+    "12,16,20",
+    "45°",
+    "85°"
+];
+
+submitBtn.addEventListener("click",calculateScore);
+
+function calculateScore(){
+    let score=0,index=0;
+    const formResult = new FormData(quizform);
+
+    for(let value of formResult.values()){
+        if(value===answers[index]){
+            score++;
+        }
+        index++;
+    }
+    output.innerText = `Your score is ${score} out of 5`;
+}
+
